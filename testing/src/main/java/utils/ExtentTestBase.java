0c4651d27a268ad477fa1d4ba34d9839aed4da97
alex <ananiev.aleksander@gmail.com>
2019-08-13 22:38:28 +0300

    demo changes

--- testing/src/main/java/utils/ExtentTestBase.java
+++ testing/src/main/java/utils/ExtentTestBase.java
@@ -3,18 +3,20 @@ package utils;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentLoggerReporter;
+import com.aventstack.extentreports.service.ExtentTestManager;
+import com.aventstack.extentreports.testng.listener.commons.ExtentTestCommons;
 
 public class ExtentTestBase {
 
 	protected static ExtentReports extent;
 	protected static ExtentLoggerReporter logger;
 	protected static ExtentTest test;
-	
-	
+	protected static ExtentTestManager manager;
+
 	protected static void initLoggerReport() {
 		logger = new ExtentLoggerReporter("testing/target");
 		extent = new ExtentReports();
 		extent.attachReporter(logger);
+
 	}
-	
 }
--- testing/src/test/java/demo/site/test/CamelotDemoTest.java
+++ testing/src/test/java/demo/site/test/CamelotDemoTest.java
@@ -11,12 +11,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Wait;
+import org.sikuli.script.Screen;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.testng.listener.ExtentITestListenerAdapter;
 
 import utils.ExtentTestBase;
@@ -31,7 +33,7 @@ public class CamelotDemoTest extends ExtentTestBase {
 	@BeforeSuite
 	private void testReportSetup() {
 		initLoggerReport();
-		System.setProperty("webdriver.chrome.driver", "webdrivers/chromedriver.exe");
+		
 	}
 	@AfterMethod
 	private void flushResults() {
@@ -40,13 +42,14 @@ public class CamelotDemoTest extends ExtentTestBase {
 	}
 	@AfterSuite
 	private void driverTierDown() {
-		driver.quit();
+		//driver.quit();
 	}
-	
-	@Test(priority =1)
+	// Method 1 : 
+	@Test(priority = 0,enabled = false)
 	private void siteLogIn() {
 		test = extent.createTest("Log in to demo site");
 		test.assignCategory("Camelot Demo Site");
+		System.setProperty("webdriver.chrome.driver", "webdrivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		WaitTool.setImplicitWait(driver, 5);
 		driver.get("https://iwg.camelot.tech/catalog");
@@ -58,28 +61,57 @@ public class CamelotDemoTest extends ExtentTestBase {
 		
 	}
 	
-	@Test(priority =2)
-	private void getGamesCount() {
-		WaitTool.waitForElement(driver, By.id("games"), 5);
-		WebElement element = driver.findElement(By.id("games"));
-		Collection<WebElement> allElements =  element.findElements(By.xpath("//div[@class='game']//div[@class='title']")) ;
-		System.out.println(allElements.size());
-		for (int i = 68; i < allElements.size(); i++) {
-			driver.findElement(By.xpath("//div[@id='games']//div["+i+"]")).click();
-			WaitTool.waitForElement(driver, By.xpath("//iframe[@id='iwg-game']"), 10);
-			try {
-				Thread.sleep(10000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				
-			}
-			driver.get("https://iwg.camelot.tech/catalog/");
-			
-		}
+	
+	
+	
+	@Test
+	private void testParent() {
+		test = extent.createTest("Parent");
+		ExtentTest node = test.createNode("node");
+		node.pass("pass");
+		System.out.println(node.getStatus());
+		node.fail("fail");
+		System.out.println(node.getStatus());
+		node.error("Skip");
+		System.out.println(node.getStatus());
+		
+		
+		
 		
 		
 	}
+	
+//	// Method 2 : 
+//	@Test(priority = 1)
+//	private void getGamesCount() {
+//		WaitTool.waitForElement(driver, By.id("games"), 5);
+//		WebElement element = driver.findElement(By.id("games"));
+//		Collection<WebElement> allElements =  element.findElements(By.xpath("//div[@class='game']//div[@class='title']")) ;
+//		System.out.println(allElements.size());
+//		for (int i = 68; i < allElements.size(); i++) {
+//			driver.findElement(By.xpath("//div[@id='games']//div["+i+"]")).click();
+//			WaitTool.waitForElement(driver, By.xpath("//iframe[@id='iwg-game']"), 10);
+//			try {
+//				Thread.sleep(10000);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//				
+//			}
+//			driver.get("https://iwg.camelot.tech/catalog/");
+//			
+//		}
+//		
+//		
+//	}
+//	@Test (priority =3)
+//	private void sikulixTest() {
+//		
+//		Screen s = new Screen();
+//		s.findWord("camelot").highlight();
+//		
+//	}
+	
 	
 	
 	
